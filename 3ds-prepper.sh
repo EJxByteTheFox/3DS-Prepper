e730bab3b789006a16f7f48b99d90040b8f086f3
Aurora Johnston (Byte the Fox) <155034127+NJxByteTheFox@users.noreply.github.com>
2024-10-13 20:53:04 -0400

    added v0.0.1

---
+++ 3ds-prepper.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+
+SSETUP=0
+
+echo "⠀⠀⠀⠀⠀⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⣸⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⢻⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣠⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⡀⠴⣾⣿⣿⣿⣤⣿⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⠀⠀⣤⣾⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣾⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢹⣿⢸⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⢻⣿⣿⡿⠿⠟⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣌⣃⣼⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⢀⣠⣤⣴⣿⣿⣍⣠⣶⣶⣶⣦⡈⢻⣿⣿⣿⣿⣿⣿⡿⠟⠋⠉⠋⠉⠛⢿⣿⣿⣿⣿⣿⠅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠈⠛⠛⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⠾⠿⣿⣿⣿⣿⣿⣤⣴⣶⣿⣿⣷⣶⣀⢹⣿⣿⣤⣶⣶⡶⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⣰⣯⣛⣉⢩⡟⠟⢿⣿⣿⣦⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⢰⠿⠿⠟⠳⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣍⣀⡤⠀⠝⢉⣹⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣦⣉⣡⣬⣙⣁⣼⣿⣿⣿⣿⣿⣿⣷⠾⠟⠻⢿⡿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢉⣹⣿⣿⣿⣿⣿⣿⣿⣉⣉⣭⣍⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠿⣷⣾⣿⣿⣿⣿⣿⣿⡿⠟⣓⣈⣅⣙⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⡟⢋⣤⣴⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠾⠿⢿⣿⣿⣿⠏⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣦⠹⡇⣾⣿⣧⢹⣿⡿⠛⢻⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣶⣤⣀⣉⣁⠈⠠⣤⣶⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⣆⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡄⠀⢰⣿⣿⣧⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣧⢹⣿⣿⣿⣆⢻⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣧⠀⣾⣿⣿⣿⣧⡀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⣿⣿⣿⣿⣿⡈⢿⣿⣿⣿⣦⣙⠛⠛⢋⡁⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣰⣿⣿⣿⣿⣿⣷⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⢀⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣁⡀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⢰⣿⣿⣿⣿⣿⣿⣿⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⢡⣠⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡄⢽⣿⣿⣿⣿⣿⣿⢌⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠆⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇"
+
+echo "|____ |  _  \/  ___|      | ___ \                              "
+echo "    / / | | |\ \`--. ______| |_/ / __ ___ _ __  _ __   ___ _ __"
+echo "    \ \ | | | \`--. \______|  __/ '__/ _ \ '_ \| '_ \ / _ \ '__|"
+echo ".___/ / |/ / /\__/ /      | |  | | |  __/ |_) | |_) |  __/ |   "
+echo "\____/|___/  \____/       \_|  |_|  \___| .__/| .__/ \___|_|   "
+echo "                                        | |   | |              "
+echo "            v0.0.0                      |_|   |_| Ellie Johnston"
+
+echo "\n Press [ENTER]"
+
+read DUMMY
+
+echo "Enter your Hax type" 
+echo "Currently supported are:"
+echo " - KartDLPhax"
+echo "========================="
+read -p "Hax type: " HAXXXX
+HAXXXX=$(echo "$HAXXXX" | tr '[:upper:]' '[:lower:]')
+
+case "$HAXXXX" in 
+    "kartdlphax" ) 
+        echo "would you like to set up a Source system for KartDLPhax"
+        read -p "[Y/n]" CHOICE
+
+        CHOICE=$(echo "$CHOICE" | tr '[:upper:]' '[:lower:]')
+
+        if [ "$CHOICE"-eq "y" ]; then
+            SSETUP=1
+            read -p "Source system folder: " SOURCE
+        fi
+    ;;
+esac
+
+if [ $SSETUP -eq 1 ]; then
+    if [ $("$HAXXXX") -eq "kartdlphax" ]; then
+        if [ -d ./3ds-prepper-tmp ]; then
+            mkdir ./3ds-prepper-tmp/kartdlp
+        fi
+        wget -q -O ./3ds-prepper-tmp/kartdlp/plugin.3gx $(curl -s https://api.github.com/repos/PabloMK7/kartdlphax/releases/latest | jq --raw-output '.assets[0] | .browser_download_url') 
+    fi
+fi
+
+read -p "Target folder: " TARGET 
+
+if [ -d ./3ds-prepper-tmp ]; then
+    rm -fr ./3ds-prepper-tmp
+fi
+
+mkdir ./3ds-prepper-tmp
+
+echo "...Downloading Luma3DS..."
+wget -q -O ./3ds-prepper-tmp/luma_latest.zip $(curl -s https://api.github.com/repos/LumaTeam/Luma3DS/releases/latest | jq --raw-output '.assets[0] | .browser_download_url') 
+echo "...Downloading boot9strap..."
+wget -q -O ./3ds-prepper-tmp/B9S_latest.zip $(curl -s https://api.github.com/repos/SciresM/boot9strap/releases/latest | jq --raw-output '.assets[3] | .browser_download_url')
+echo "...Downloading FBI..."
+wget -q -O ./3ds-prepper-tmp/FBI.3dsx https://github.com/nh-server/FBI-NH/releases/download/2.6.1/FBI.3dsx
+wget -q -O ./3ds-prepper-tmp/FBI.cia https://github.com/nh-server/FBI-NH/releases/download/2.6.1/FBI.cia
+echo "...Downloading Universal Updater..."
+wget -q -O ./3ds-prepper-tmp/UUP.cia $(curl -s https://api.github.com/repos/Universal-Team/Universal-Updater/releases/latest | jq --raw-output '.assets[1] | .browser_download_url')
+echo "...Downloading GodMode9..."
+wget -q -O ./3ds-prepper-tmp/GM9_latest.zip $(curl -s https://api.github.com/repos/d0k3/GodMode9/releases/latest | jq --raw-output '.assets[0] | .browser_download_url')
+
+if [ ! -d ./3ds-prepper-tmp/luma_extr ]; then
+    mkdir ./3ds-prepper-tmp/luma_extr
+fi
+
+if [ ! -d ./3ds-prepper-tmp/b9s_extr ]; then
+    mkdir ./3ds-prepper-tmp/b9s_extr
+fi
+
+if [ ! -d ./3ds-prepper-tmp/gm9_extr ]; then
+    mkdir ./3ds-prepper-tmp/gm9_extr
+fi
+
+echo "...extracting Luma3DS..."
+unzip -q -d ./3ds-prepper-tmp/luma_extr ./3ds-prepper-tmp/luma_latest.zip
+echo "...extracting boot9strap..."
+unzip -q -d ./3ds-prepper-tmp/b9s_extr ./3ds-prepper-tmp/B9S_latest.zip
+echo "...extracting GodMode9..."
+unzip -q -d ./3ds-prepper-tmp/gm9_extr ./3ds-prepper-tmp/GM9_latest.zip
+
+cd ./3ds-prepper-tmp
+
+mkdir NEWSD
+
+echo "...finalizing..."
+cp ./luma_extr/boot.firm ./NEWSD
+cp ./luma_extr/boot.3dsx ./NEWSD
+mkdir ./NEWSD/luma
+mkdir ./NEWSD/luma/payloads
+cp ./gm9_extr/GodMode9.firm ./NEWSD/luma/payloads
+mkdir ./NEWSD/boot9strap
+mkdir ./NEWSD/3ds
+mkdir ./NEWSD/CIAs
+cp ./FBI.3dsx ./NEWSD/3ds
+cp ./FBI.cia ./NEWSD/CIAs
+cp ./UUP.cia ./NEWSD/CIAs
+cp ./b9s_extr/boot9strap.firm ./NEWSD/boot9strap
+cp ./b9s_extr/boot9strap.firm.sha ./NEWSD/boot9strap
+
+if [ ! -d ../$TARGET ]; then 
+    mkdir ../$TARGET
+fi
+
+cp -fr ./NEWSD/* ../$TARGET
+
+touch ../$TARGET/info.txt
+echo "Generated by 3ds prepper! (0.0.1)" > ../$TARGET/info.txt
+cd ..
+rm -fr ./3ds-prepper-tmp
